caf8c2837265cc7ee90a50b687ff9046e92f3115
Justin Ross <jross@apache.org>
2017-03-04 12:47:43 -0800

    Checkpoint

--- python/qtools.py
+++ python/qtools.py
@@ -37,17 +37,19 @@ except ImportError:
     from urlparse import urlparse as _urlparse
 
 class _Application(object):
-    def __init__(self, name=None):
+    def __init__(self, name, handler_cls):
         self.name = name
+        self.container = Container(handler_cls(self))
+
         self.quiet = False
         self.verbose = False
 
-        if self.name is None:
-            self.name = _os.path.split(_sys.argv[0])[1]
-
     def __repr__(self):
-        return _pencil.format_repr(self, self.domain)
+        return _pencil.format_repr(self)
 
+    def run(self):
+        self.container.run()
+    
     def debug(self, message, *args):
         if not self.verbose:
             return
@@ -66,89 +68,96 @@ class _Application(object):
 
         print(message)
 
-class SendHandler(MessagingHandler):
+class _ApplicationHandler(MessagingHandler):
+    def __init__(self, app):
+        super(_ApplicationHandler, self).__init__()
+
+        self.app = app
+
+class PingClient(_Application):
+    def __init__(self, domain):
+        super(PingClient, self).__init__("qping", _PingHandler)
+
+        self.domain = domain
+
+class _PingHandler(_ApplicationHandler):
+    def on_start(self, event):
+        event.container.connect(self.app.domain, allowed_mechs="ANONYMOUS")
+
+    def on_connection_opened(self, event):
+        self.app.notice("Connected to domain '{}'", self.app.domain)
+
+        event.connection.close()
+
+class SendClient(_Application):
     def __init__(self, address, message_body):
-        super(SendHandler, self).__init__()
+        super(SendClient, self).__init__("qsend", _SendHandler)
 
         self.address = address
         self.message_body = message_body
+        
+class _SendHandler(_ApplicationHandler):
+    def __init__(self, app):
+        super(_SendHandler, self).__init__(app)
 
         self.sent = False
-        self.verbose = False
-
-    def print(self, message, *args):
-        if not self.verbose:
-            return
-
-        message = "qsend: {}".format(message)
-
-        print(message.format(*args))
 
     def on_start(self, event):
-        host, port, path = _parse_address(self.address)
+        host, port, path = _parse_address(self.app.address)
         domain = "{}:{}".format(host, port)
 
-        # XXX
-        # - Connection is not anonymous by default
-        # - Can't set mechs on create_sender or on container
-        # - Requires this two-step procedure
         conn = event.container.connect(domain, allowed_mechs="ANONYMOUS")
         event.container.create_sender(conn, path)
 
-        self.print("Created sender for target address '{}'", path)
+        self.app.notice("Created sender for target address '{}'", path)
 
     def on_sendable(self, event):
         if self.sent:
             return
 
-        message = Message(self.message_body)
+        message = Message(self.app.message_body)
         event.sender.send(message)
 
-        self.print("Sent message '{}'", self.message_body)
+        self.app.notice("Sent message '{}'", self.app.message_body)
 
         self.sent = True
 
     def on_accepted(self, event):
         event.connection.close()
 
-class ReceiveHandler(MessagingHandler):
+class ReceiveClient(_Application):
     def __init__(self, address, messages):
-        super(ReceiveHandler, self).__init__()
+        super(ReceiveClient, self).__init__("qreceive", _ReceiveHandler)
 
         self.address = address
         self.messages = messages
+        
+class _ReceiveHandler(_ApplicationHandler):
+    def __init__(self, app):
+        super(_ReceiveHandler, self).__init__(app)
 
-        self.verbose = False
         self.count = 0
 
-    def print(self, message, *args):
-        if not self.verbose:
-            return
-
-        message = "qreceive: {}".format(message)
-
-        print(message.format(*args))
-
     def on_start(self, event):
-        host, port, path = _parse_address(self.address)
+        host, port, path = _parse_address(self.app.address)
         domain = "{}:{}".format(host, port)
 
         conn = event.container.connect(domain, allowed_mechs="ANONYMOUS")
         event.container.create_receiver(conn, path)
 
-        self.print("Created receiver for source address '{}'", path)
+        self.app.notice("Created receiver for source address '{}'", path)
 
     def on_message(self, event):
-        if self.count == self.messages:
+        if self.count == self.app.messages:
             return
 
-        self.print("Received message '{}'", event.message.body)
+        self.app.notice("Received message '{}'", event.message.body)
 
         print(event.message.body)
 
         self.count += 1
 
-        if self.count == self.messages:
+        if self.count == self.app.messages:
             event.connection.close()
 
 class DrainHandler(MessagingHandler):
@@ -192,14 +201,14 @@ class DrainHandler(MessagingHandler):
             event.connection.close()
 
 class _BrokerQueue(object):
-    def __init__(self, broker, address):
-        self.broker = broker
+    def __init__(self, app, address):
+        self.app = app
         self.address = address
 
         self.messages = _collections.deque()
         self.consumers = list()
 
-        self.broker.notice("Creating {}", self)
+        self.app.notice("Creating {}", self)
 
     def __repr__(self):
         return "queue '{}'".format(self.address)
@@ -209,7 +218,7 @@ class _BrokerQueue(object):
         assert link not in self.consumers
 
         m = "Adding consumer for '{}' to {}"
-        self.broker.notice(m, link.connection.remote_container, self)
+        self.app.notice(m, link.connection.remote_container, self)
 
         self.consumers.append(link)
 
@@ -217,7 +226,7 @@ class _BrokerQueue(object):
         assert link.is_sender
 
         m = "Removing consumer for '{}' from {}"
-        self.broker.notice(m, link.connection.remote_container, self)
+        self.app.notice(m, link.connection.remote_container, self)
 
         try:
             self.consumers.remove(link)
@@ -238,25 +247,24 @@ class _BrokerQueue(object):
 
             link.send(message)
 
-class _BrokerHandler(MessagingHandler):
-    def __init__(self, broker):
-        super(_BrokerHandler, self).__init__()
+class _BrokerHandler(_ApplicationHandler):
+    def __init__(self, app):
+        super(_BrokerHandler, self).__init__(app)
 
-        self.broker = broker
         self.queues = dict()
 
         self.verbose = False
 
     def on_start(self, event):
-        self.acceptor = event.container.listen(self.broker.domain)
+        self.acceptor = event.container.listen(self.app.domain)
 
-        self.broker.notice("Listening on '{}'", self.broker.domain)
+        self.app.notice("Listening on '{}'", self.app.domain)
 
     def get_queue(self, address):
         try:
             queue = self.queues[address]
         except KeyError:
-            queue = self.queues[address] = _BrokerQueue(self.broker, address)
+            queue = self.queues[address] = _BrokerQueue(self.app, address)
 
         return queue
 
@@ -288,20 +296,20 @@ class _BrokerHandler(MessagingHandler):
 
     def on_connection_opening(self, event):
         m = "Opening connection from '{}'"
-        self.broker.notice(m, event.connection.remote_container)
+        self.app.notice(m, event.connection.remote_container)
 
         # XXX I think this should happen automatically
         event.connection.container = event.container.container_id
 
     def on_connection_closing(self, event):
         m = "Closing connection from '{}'"
-        self.broker.notice(m, event.connection.remote_container)
+        self.app.notice(m, event.connection.remote_container)
 
         self.remove_consumers(event.connection)
 
     def on_disconnected(self, event):
         m = "Disconnected from {}"
-        self.broker.notice(m, event.connection.remote_container)
+        self.app.notice(m, event.connection.remote_container)
 
         self.remove_consumers(event.connection)
 
@@ -328,16 +336,12 @@ class _BrokerHandler(MessagingHandler):
 
 class Broker(_Application):
     def __init__(self, domain):
-        super(Broker, self).__init__()
+        super(Broker, self).__init__("qbroker", _BrokerHandler)
 
         self.domain = domain
-        self.container = Container(_BrokerHandler(self))
 
     def __repr__(self):
         return _pencil.format_repr(self, self.domain)
-
-    def run(self):
-        self.container.run()
 
 class QtoolsError(Exception):
     pass
