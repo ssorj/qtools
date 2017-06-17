c0600b63fe3fe69c1ed6fdad3bb44577e81de521
Justin Ross <jross@apache.org>
2017-06-17 07:32:31 -0700

    Add support for multiple messages and interactive message input

--- python/qtools/send.py
+++ python/qtools/send.py
@@ -23,13 +23,15 @@ from __future__ import print_function
 from __future__ import unicode_literals
 from __future__ import with_statement
 
+import collections as _collections
 import proton as _proton
 import proton.handlers as _handlers
 import proton.reactor as _reactor
+import threading as _threading
 
 from .common import *
 
-_description = "Send an AMQP message"
+_description = "Send AMQP messages"
 
 class SendCommand(Command):
     def __init__(self, home_dir):
@@ -37,52 +39,123 @@ class SendCommand(Command):
 
         self.parser.description = _description
 
-        self.parser.add_argument("address", metavar="ADDRESS-URL")
-        self.parser.add_argument("body", metavar="MESSAGE-BODY", nargs="?")
+        self.parser.add_argument("url", metavar="ADDRESS-URL",
+                                 help="The location of a message queue")
+        self.parser.add_argument("message", metavar="MESSAGE", nargs="*",
+                                 help="The message content")
+        self.parser.add_argument("--interactive", action="store_true",
+                                 help="Prompt for messages on the console")
 
         self.add_common_arguments()
+
+        handler = _SendHandler(self)
+
+        self.container = _reactor.Container(handler)
+        self.events = _reactor.EventInjector()
+        self.messages = _collections.deque()
+        self.console_input_thread = _ConsoleInputThread(self)
 
     def init(self):
         super(SendCommand, self).init()
 
-        self.address = self.args.address
-        self.body = self.args.body
-
         self.init_common_attributes()
 
-    def run(self):
-        handler = _SendHandler(self)
-        container = _reactor.Container(handler)
+        self.url = self.args.url
+        self.command_line_messages = self.args.message
+        self.interactive = self.args.interactive
 
-        container.run()
+        for message in self.command_line_messages:
+            pmessage = _proton.Message(unicode(message))
+            self.messages.appendleft(pmessage)
+
+        if self.interactive:
+            self.quiet = True
+        else:
+            self.messages.appendleft(None)
+
+    def run(self):
+        if self.interactive:
+            self.container.selectable(self.events)
+            self.console_input_thread.start()
+
+        self.container.run()
+
+class _ConsoleInputThread(_threading.Thread):
+    def __init__(self, command):
+        _threading.Thread.__init__(self)
+
+        self.command = command
+        self.daemon = True
+
+    def run(self):
+        while True:
+            try:
+                body = raw_input("> ")
+            except EOFError:
+                self.command.messages.appendleft(None)
+                self.command.events.trigger(_reactor.ApplicationEvent("input"))
+                print()
+
+                break
+
+            message = _proton.Message(unicode(body))
+
+            self.command.messages.appendleft(message)
+            self.command.events.trigger(_reactor.ApplicationEvent("input"))
 
 class _SendHandler(_handlers.MessagingHandler):
     def __init__(self, command):
         super(_SendHandler, self).__init__()
 
         self.command = command
-
-        self.sent = False
+        self.connection = None
+        self.sender = None
+        self.stop_requested = False
 
     def on_start(self, event):
-        host, port, path = parse_address_url(self.command.address)
+        host, port, path = parse_address_url(self.command.url)
         domain = "{}:{}".format(host, port)
 
-        conn = event.container.connect(domain, allowed_mechs=b"ANONYMOUS")
-        event.container.create_sender(conn, path)
+        self.connection = event.container.connect(domain, allowed_mechs=b"ANONYMOUS")
+        self.sender = event.container.create_sender(self.connection, path)
 
         self.command.notice("Created sender for target address '{}'", path)
 
     def on_sendable(self, event):
-        if self.sent:
-            return
+        self.send_message()
 
-        message = _proton.Message(unicode(self.command.body))
-        event.sender.send(message)
-
-        self.command.notice("Sent message '{}'", self.command.body)
-
-        self.sent = True
+    def on_input(self, event):
+        self.send_message()
 
     def on_accepted(self, event):
-        event.connection.close()
+        if self.stop_requested:
+            self.close()
+
+    def send_message(self):
+        if self.stop_requested:
+            return
+
+        try:
+            message = self.command.messages.pop()
+        except IndexError:
+            return
+
+        if message is None:
+            if self.sender.unsettled == 0:
+                self.close()
+            else:
+                self.stop_requested = True
+
+            return
+
+        if not self.sender.credit:
+            self.command.messages.append(message)
+            return
+
+        self.sender.send(message)
+
+        self.command.notice("Sent message '{}'", message.body)
+
+    def close(self):
+        self.connection.close()
+        self.command.events.close()
